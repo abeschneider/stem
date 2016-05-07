b4463e2851cab571609b22e6829eeb31e18fd1ae
Abraham Schneider <abe.schneider@gmail.com>
2016-05-07 07:26:45 -0400

    Updated playground code.

--- StemPlayground.playground/Contents.swift
+++ StemPlayground.playground/Contents.swift
@@ -8,34 +8,30 @@ import XCPlayground
 
 //: Convenience aliases
 //: -------------------
-/*typealias Vec = Vector<NativeStorage<Float>>
-typealias RowVec = RowVector<NativeStorage<Float>>
-typealias ColVec = ColumnVector<NativeStorage<Float>>
-typealias Mat = Matrix<NativeStorage<Float>>
+typealias F = NativeStorage<Float>
+typealias I = NativeStorage<Int>
 
 //: Creating vectors
 //: ----------------
-let v1 = Vec([1, 2, 3, 4])
-print("\(v1)")
+let v1:Tensor<F> = colvector([1, 2, 3, 4])
+String(v1)
 
-let v2 = ColVec([1, 2, 3, 4])
-print("\(v2)")
+let v2:Tensor<F> = rowvector([1, 2, 3, 4])
+String(v2)
 
-let v3 = RowVec([1, 2, 3, 4])
-print("\(v3)")
+let v3:Tensor<F> = vector([1, 2, 3, 4], axis: 0)
+String(v3)
 
-let v4 = Vec([1, 2, 3, 4], axis: 0)
-print("\(v4)")
+let v4:Tensor<F> = vector([1, 2, 3, 4], axis: 1)
+String(v4)
 
-let v5 = Vec([1, 2, 3, 4], axis: 1)
-print("\(v5)")
+let v5:Tensor<F> = vector([1, 2, 4, 5], axis:2)
+String(v5)
 
-let v6 = Vec([1, 2, 3, 4], axis: 2)
-print("\(v6)")
 
 // integer vectors
-let iv1 = RowVector<NativeStorage<Int>>([1, 2, 3, 4, 5])
-print("\(iv1)")
+let iv1:Tensor<I> = rowvector([1, 2, 3, 4, 5])
+String(iv1)
 
 //: Vector indexing
 //: ---------------
@@ -48,11 +44,11 @@ v1[0] = 10
 v2[1] = 20
 
 // ranges
-let vr1 = v1[0...2]
-print("\(vr1)")
+let vr1 = v1[1, 0...2]
+String(vr1)
 
-vr1[0...2] = Vec([1, 2, 3])
-print("\(vr1)")
+vr1[0...2] = colvector([1, 2, 3])
+String(vr1)
 
 //: Vector operations
 //: -----------------
@@ -61,24 +57,26 @@ let v7 = v1+v1
 let v8 = 0.5*v1
 let v9 = v1**2
 
-let v10 = RowVec([2, 2, 2, 2])
+let v10:Tensor<F> = rowvector([2, 2, 2, 2])
 
 
 // Hadamard product
 let v11 = v10*v2
+String(v11)
 
 // dot product
-let v12 = v10⊙(v2+v2)
+let v12:Float = v10⊙(v2+v2)
+String(v12)
 
 // outer product
 let v13 = v10⊗(v2+v2)
-print("\(v13)")
+String(v13)
 
 
 //: Matrix creation
 //: ---------------
-let m1 = Mat([[1, 2, 3], [4, 5, 6]])
-let m2 = Mat(rows: 3, cols: 3)
+let m1:Tensor<F> = tensor([[1, 2, 3], [4, 5, 6]])
+let m2:Tensor<F> = tensor(Extent(3, 3))
 
 //: Matrix indexing
 //: ---------------
@@ -87,7 +85,7 @@ let d4:Float = m1[1, 2]
 let vr2 = m1[0..<2, 1..<3]
 print("\(vr2)")
 
-m2[0..<2, 1..<3] = Mat([[1, 1], [1, 1]])
+m2[0..<2, 1..<3] = tensor([[1, 1], [1, 1]])
 String((m2))
 
 String(m2[all, 0])
@@ -100,14 +98,15 @@ String(m2[all, 2])
 //: -----------------
 String(m1)
 
-let v14 = ColVec([1, 1, 1])
-let v15 = m1⊙v14
+let v14:Tensor<F> = colvector([1, 1, 1])
+let v15:Tensor<F> = m1⊙v14
+String(v15)
 
-let m3 = Mat([[1, 2, 3], [1, 2, 3]])
-let m4 = m1⊙m3.transpose()
+let m3:Tensor<F> = tensor([[1, 2, 3], [1, 2, 3]])
+let m4:Tensor<F> = m1⊙m3.transpose()
 String(m4)
 
 
 let s = Extent(3)
 let s2 = Extent(s, over: 5)
-print("\(s2.dims)")*/
+s2.dims
