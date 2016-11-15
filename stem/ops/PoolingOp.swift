d3a458481f9f18d699845942ab82ec04782d5697
Abraham Schneider <abe.schneider@gmail.com>
2016-11-15 07:05:46 -0500

    More work on pooling layer.

--- stem/ops/PoolingOp.swift
+++ stem/ops/PoolingOp.swift
@@ -8,25 +8,54 @@
 
 import Foundation
 
+func max<S:Storage>(input:Tensor<S>) -> Int {
+    let flattenedInput = ravel(input)
+    var best = S.ElementType(10000)
+    var bestIndex:Int = 0
+    for index in flattenedInput.indices() {
+        let value = input[index]
+        if value > best {
+            best = value
+            bestIndex = index[0]
+        }
+    }
+    
+    return bestIndex
+}
+
+func min<S:Storage>(input:Tensor<S>) -> Int {
+    let flattenedInput = ravel(input)
+    var best = S.ElementType(-10000)
+    var bestIndex:Int = 0
+    for index in flattenedInput.indices() {
+        let value = input[index]
+        if value < best {
+            best = value
+            bestIndex = index[0]
+        }
+    }
+    
+    return bestIndex
+}
+
 open class PoolingOp<S:Storage>: Op<S> where S.ElementType:FloatNumericType {
     open var _input:Tensor<S> { return inputs[0].output() }
     
     var poolingSize:Extent
     var stride:Extent
-    var initial:S.ElementType
-    var evalFn:(_ a:S.ElementType, _ b:S.ElementType) -> Bool
+    var evalFn:(_ input:Tensor<S>) -> Int
+    var indices:Tensor<NativeStorage<Int>>
 
     public init(input: Op<S>,
                 poolingSize:Extent,
                 stride:Extent,
-                initial:S.ElementType,
-                evalFn:@escaping (_ a:S.ElementType, _ b:S.ElementType) -> Bool)
+                evalFn:@escaping (_ input:Tensor<S>) -> Int)
     {
         self.poolingSize = poolingSize
         self.stride = stride
-        self.initial = initial
         self.evalFn = evalFn
         
+        indices = Tensor<NativeStorage<Int>>()
         super.init(inputs: ["input"], outputs: ["output"])
         connect(from: input, "output", to: self, "input")
         
@@ -41,18 +70,19 @@ open class PoolingOp<S:Storage>: Op<S> where S.ElementType:FloatNumericType {
     open override func apply() {
         let width = _input.shape[0] / stride[0]
         let height = _input.shape[0] / stride[1]
-        var best = initial
+        
+        if indices.shape != Extent(width, height) {
+            indices.resize(Extent(width, height))
+        }
         
         for i in 0..<width {
             for j in 0..<height {
-                for k in 0..<poolingSize[0] {
-                    for l in 0..<poolingSize[1] {
-                        let v = _input[i+k, j+l]
-                        if evalFn(v, best) {
-                            best = v
-                        }
-                    }
-                }
+                let row_start = i*stride[0]
+                let row_end = row_start + stride[0]
+                let col_start = j*stride[1]
+                let col_end = col_start + stride[1]
+                let view = _input[row_start..<row_end, col_start..<col_end]
+                indices[i, j] = evalFn(view)
             }
         }
     }
