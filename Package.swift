f9fc878cdbd4de38016958b7ce50b740a2a702a7
Abraham Schneider <abeschneider@MacBook-Pro.home>
2016-11-27 21:45:10 -0500

    forgot to add GZip dependency

--- Package.swift
+++ Package.swift
@@ -9,5 +9,6 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,24)),
+    .Package(url: "https://github.com/1024jp/GzipSwift.git", Version(3, 1, 2))
   ]
 )
