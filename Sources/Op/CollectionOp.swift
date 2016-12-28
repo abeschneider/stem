bd83ad46c1de705248db9149cf9d8a258c64ce16
Abraham Schneider <abeschneider@Abrahams-MacBook-Pro.local>
2016-12-28 13:57:36 -0500

    Moved SequentialOp and ColletionOp out of graph.swift.

---
+++ Sources/Op/CollectionOp.swift
@@ -0,0 +1,182 @@
+//
+//  CollectionOp.swift
+//  stem
+//
+//  Created by Abraham Schneider on 12/26/16.
+//
+//
+
+import Foundation
+import Tensor
+
+open class CollectionOp<S:Storage>: Op<S>, Sequence {
+    open var ops:[Op<S>]
+    open var ordering:AnyOrdering<S>
+    
+    public init<T:Ordering>(
+        ops:[Op<S>],
+        inputs:[Op<S>],
+        outputs:[Op<S>],
+        ordering:T) where T.StorageType==S
+    {
+        self.ordering = AnyOrdering<S>(ordering)
+        self.ops = ops
+        super.init(inputs: ["input"], outputs: ["output"])
+        
+        if inputs.count > 0 {
+            connect(from: inputs, "output", to: self, "input")
+        }
+        
+        self.outputs["output"] = outputs.map { $0.output }
+        
+        //        setAction("input", action: self.inputSet)
+    }
+    
+    // required for Copyable
+    public required init(op:Op<S>, shared:Bool) {
+        let cop = op as! CollectionOp<S>
+        ordering = AnyOrdering<S>(cop.ordering)
+        ops = cop.ops.map { copy(op: $0, shared: shared) }
+        
+        super.init(inputs: ["input"], outputs: ["outputs"])
+        outputs["output"] = [Tensor<S>(op.output.shape)]
+    }
+    
+    //    func inputSet(_ label:String, value:[Op<S>]) {
+    //        connect(from: value[0], to: ops[0])
+    //    }
+    
+    open override func apply() {
+        for op in ordering.traversal(ops) {
+            op.apply()
+        }
+    }
+    
+    open func makeIterator() -> AnyIterator<Op<S>> {
+        return ordering.traversal(ops).makeIterator()
+    }
+    
+    open override func reset() {
+        fill(output, value: 0)
+    }
+    
+    open override var description: String {
+        let className = String(describing: Mirror(reflecting: self).subjectType)
+        let inputShapes = inputs["input"]!.map {
+            let output:Tensor<S> = $0.output()
+            return String(describing: output.shape.dims)
+            }.joined(separator: ", ")
+        
+        let value = ops.map { String(describing: $0) }.joined(separator: "\n\t")
+        return "<\(className): inputs:\(inputShapes), outputs:\(output.shape.dims)> {\n\t\(value)\n}"
+    }
+}
+
+open class CollectionGradient<S:Storage>: Op<S>, Gradient {
+    public typealias StorageType = S
+    open var ops:[Op<StorageType>] = []
+    var ordering:AnyOrdering<S>
+    
+    open var _input:Tensor<S> { return inputs[1].output() }
+    open var _gradOutput:Tensor<S> { return inputs[2].output() }
+    
+    public required init(op:CollectionOp<S>) {
+        ordering = op.ordering
+        super.init(inputs: ["op", "inputs", "gradOutput"], outputs: ["output"])
+        
+        outputs["output"] = [Tensor<S>()]
+        
+        // start with outputs, and work backwards
+        createBackwardsGraph(op.ops)
+        //        output = ops.last!.output
+        
+        setAction("gradOutput", action: self.gradOutputSet)
+    }
+    
+    public init<T:Ordering>(
+        ops:[Op<S>],
+        inputs:[Op<S>],
+        outputs:[Op<S>],
+        ordering:T) where T.StorageType==S
+    {
+        self.ordering = AnyOrdering<S>(ordering)
+        self.ops = ops
+        super.init(inputs: ["op", "input", "gradOutput"], outputs: ["outputs"])
+        //        connect(from: outputs, "output", to: self, "gradOutput")
+        
+        self.outputs["output"] = outputs.map { $0.output }
+        setAction("gradOutput", action: self.gradOutputSet)
+    }
+    
+    required public init(op: Op<S>, shared: Bool) {
+        fatalError("init(op:shared:) has not been implemented")
+    }
+    
+    func createBackwardsGraph(_ ops:[Op<S>]) {
+        // convert all ops -> opGradient
+        var gradOps:[Int:Op<S>] = [:]
+        
+        for op in ops.reversed() {
+            if let diffOp = op as? Differentiable {
+                let gradOp = (diffOp.gradient() as! Op<S>)
+                gradOps[op.id] = gradOp
+                self.ops.append(gradOp)
+            }
+        }
+        
+        // connect all (op1 -> op2) to (op2Gradient -> op1Gradient)
+        for op in ops {
+            if let opInputs:[InputType<S>] = op.inputs["input"] {
+                for input in opInputs {
+                    if  let fromOp = gradOps[op.id], let toOp = gradOps[input.op!.id] {
+                        connect(from: fromOp, to: toOp, "gradOutput")
+                    }
+                }
+            }
+        }
+    }
+    
+    func gradOutputSet(_ key:String, value:[Op<S>]) {
+        connect(from: value, "output", to: ops.first!, "gradOutput")
+    }
+    
+    open override func apply() {
+        print("CollectionGrad: \(_gradOutput)")
+        
+        var lastOp:Op<S>?
+        for op in ordering.traversal(ops) {
+            op.apply()
+            lastOp = op
+        }
+        
+        if let op:Op<S> = lastOp {
+            // TODO: make this a function (part of copy?)
+            // Also: is it necessary to copy, or should we
+            // just point?
+            if output.shape != op.output.shape {
+                output.resize(op.output.shape)
+            }
+            
+            copy(from: op.output, to: output)
+            print("graph: \(output)")
+        }
+    }
+    
+    open override func reset() {
+        ops.forEach { $0.reset() }
+        fill(output, value: 0)
+    }
+    
+    open override var description: String {
+        let className = String(describing: Mirror(reflecting: self).subjectType)
+        
+        let value = ops.map { String(describing: $0) }.joined(separator: "\n\t")
+        return "<\(className): inputs=?, outputs=\(output.shape.dims)> {\n\t\(value)\n}"
+    }
+}
+
+extension CollectionOp:Differentiable {
+    public func gradient() -> GradientType {
+        return CollectionGradient<S>(op: self)
+    }
+}
---
+++ Sources/Op/SequentialOp.swift
@@ -0,0 +1,104 @@
+//
+//  SequentialOp.swift
+//  stem
+//
+//  Created by Abraham Schneider on 12/26/16.
+//
+//
+
+import Foundation
+import Tensor
+
+open class SequentialOp<S:Storage>: CollectionOp<S> {
+    public init(_ ops:[Op<S>], modify:Bool=true) {
+        if modify  {
+            for i in 0..<ops.count-1 {
+                print("Connecting \(ops[i].id) to \(ops[i+1].id)")
+                connect(from: ops[i], to: ops[i+1])
+            }
+        }
+        
+        super.init(ops: ops,
+                   inputs: [ops.first!],
+                   outputs: [ops.last!],
+                   ordering: SequentialOrdering())
+        
+        setAction("input", action: self.inputSet)
+    }
+    
+    // FIXME: had to get rid of `modify` to avoid compiler segfaulting
+    public convenience init(_ ops:Op<S>...) {
+        self.init(ops)
+    }
+    
+    public  init() {
+        super.init(ops: [],
+                   inputs: [],
+                   outputs: [],
+                   ordering: SequentialOrdering())
+        
+        setAction("input", action: self.inputSet)
+    }
+    
+    // required for Copyable
+    public required convenience init(op:Op<S>, shared:Bool) {
+        let cop = op as! SequentialOp<S>
+        let ops = cop.ops.map { copy(op: $0, shared: shared) }
+        
+        self.init(ops, modify: true)
+    }
+    
+    func inputSet(_ label:String, input:[Op<S>]) {
+//        for op in ops {
+//            if let action = op.inputActions[label] {
+//                action(label, input)
+//            }
+//        }
+        
+//        if let first = ops.first {
+//            if let action = first.inputActions[label] {
+//                connect(from: input, to: first)
+//                action(label, input)
+//            }
+//        }
+        
+        if let op = ops.first {
+            let inputValues:[InputType<S>] = inputs["input"]!
+            op.inputs["input"] = inputValues
+            if let action = op.inputActions[label] {
+                action(label, input)
+            }
+        }
+    }
+    
+    public func append(_ op:Op<S>) {
+        //        if let last = ops.last!
+        let lastOp = ops.last
+        ops.append(op)
+        
+        if let last = lastOp {
+            connect(from: last, "output", to: op, "input")
+        } else {
+            let input:[InputType<S>] = inputs["input"]!
+            op.inputs["input"] = input
+        }
+        
+        outputs["output"] = [op.output]
+    }
+}
+
+// TODO: previous versions of Swift didn't allow override of an extension.
+// it seems to compile now. Should finish this, as it allows a more efficient
+// method to calculate the gradient
+//public class SequentialGradient<S:Storage>: CollectionGradient<S> {
+//    public required init(op:CollectionOp<S>) {
+//        super.init(op: op)
+//
+//        // TODO: can likely make init more efficient by traversing
+//        // list in reverse order instead of using processOutputs
+//    }
+//
+//    required public init(op: Op<S>, shared: Bool) {
+//        fatalError("init(op:shared:) has not been implemented")
+//    }
+//}
